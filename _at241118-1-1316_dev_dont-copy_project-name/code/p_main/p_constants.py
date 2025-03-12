78494081883c351ea1ad26665350d370eab8d16f
Peter Gall <gp.bitmiller@gmail.com>
2025-03-12 06:38:46 +0100

    at250312

--- _at241118-1-1316_dev_dont-copy_project-name/code/p_main/p_constants.py
+++ _at241118-1-1316_dev_dont-copy_project-name/code/p_main/p_constants.py
@@ -11,16 +11,25 @@ PROJECT_TEMPLATE_VERSION = "@241118-1-1316"
 # Program run command in Android:Termux:Python :
 RUN_COMMAND = "python /storage/emulated/0/BitMiller/Pradhana/Dropbox/bitmiller_hu/progs/python_for_termux/__base_projects/_at241118-1-1316_dev_dont-copy_project-name/code/main.py"
 
-# Programs' root in Android:Termux:Python :
+#####
+
+PRADHANA_ROOT = "/storage/emulated/0/BitMiller/Pradhana/"
+
+DROPBOX_PATHCHUNK = "Dropbox/"
+
+NS_DROPBOX_PATHCHUNK = "_no_sync_Dropbox/"
+
+PY_PROGRAMS_ROOT_PATHCHUNK = "bitmiller_hu/progs/python_for_termux/"
+
+PROJECT_ROOT_PATHCHUNK = "__base_projects/_at241118-1-1316_dev_dont-copy_project-name"
+
+#####
+
 PY_PROGRAMS_ROOT = "/storage/emulated/0/BitMiller/Pradhana/Dropbox/bitmiller_hu/progs/python_for_termux"
 
-PRADHANA_ROOT = "/storage/emulated/0/BitMiller/Pradhana"
+PROJECT_ROOT = PRADHANA_ROOT + DROPBOX_PATHCHUNK + PY_PROGRAMS_ROOT_PATHCHUNK + PROJECT_ROOT_PATHCHUNK
 
-PY_PROGRAMS_PATHCHUNK = "bitmiller_hu/progs/python_for_termux"
-
-PROJECT_PATHCHUNK = "__base_projects/_at241118-1-1316_dev_dont-copy_project-name"
-
-PROJECT_ROOT = PRADHANA_ROOT + "/Dropbox/" + PY_PROGRAMS_PATHCHUNK + "/" + PROJECT_PATHCHUNK
+NS_PROJECT_ROOT = PRADHANA_ROOT + NS_DROPBOX_PATHCHUNK + PY_PROGRAMS_ROOT_PATHCHUNK + PROJECT_ROOT_PATHCHUNK
 
 # Change working directory to project's root directory
 prj_root = run_cmd[7:]
@@ -28,11 +37,10 @@ prj_root = os.path.dirname(prj_root)
 prj_root = os.path.dirname(prj_root)
 os.chdir(prj_root)
 
-OUTPUT_LIGHT_PATH = PY_PROGRAMS_ROOT + "/" + PROJECT_PATHCHUNK + "/_no_sync/output_light"
+OUTPUT_LIGHT_PATH = PROJECT_ROOT + "/_no_sync/output_light"
 
-# This path is to reach pradhana/no_dropbox
-#OUTPUT_HEAVY_PATH = PY_PROGRAMS_ROOT + "/_no_sync/output_light"
+OUTPUT_HEAVY_PATH = PROJECT_ROOT + "/_no_sync/output_light"
 
-COMMON_MODULES = "/storage/emulated/0/BitMiller/Pradhana/Dropbox/bitmiller_hu/progs/python_for_termux/__common"
+COMMON_MODULES_PATH = "/storage/emulated/0/BitMiller/Pradhana/Dropbox/bitmiller_hu/progs/python_for_termux/__common"
 
 
